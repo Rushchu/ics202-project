b70b3a6d726789e005fae68efa5948dbcb9adc47
Rush <haidar.abugurain@gmail.com>
2023-04-30 12:39:06 +0300

    create dictionary class

---
+++ src/Classes/Dictionary.java
@@ -0,0 +1,33 @@
+package Classes;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Dictionary {
+
+    private AVLTree<String> dictionary = new AVLTree<>();
+
+    public Dictionary() {
+    }
+
+    public Dictionary(String word) {
+        this.dictionary.insert(word);
+    }
+
+    public Dictionary(File file) {
+        loadFile(file);
+    }
+
+    private void loadFile(File file) {
+        try (Scanner readFile = new Scanner(file);) {
+            String value = readFile.nextLine();
+            while (value != null) {
+                if (!this.dictionary.search(value))
+                    this.dictionary.insert(value);
+                value = readFile.nextLine();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
